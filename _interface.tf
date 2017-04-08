475756589a0eda5349a88b7fed3a7d75f8eb7ba8
Phil Watts <phil@hashicorp.com>
2017-04-07 22:38:27 -0400

    updated for hashistack use

--- _interface.tf
+++ _interface.tf
@@ -1,35 +1,80 @@
-variable "acm_domain" {}
-variable "alb_port" {}
-variable "alb_protocol" {}
-variable "ssl_policy" { default = "ELBSecurityPolicy-2016-08" }
+variable "consul_acm_domain" {}
+variable "vault_acm_domain" {}
+variable "nomad_acm_domain" {}
+variable "vault_alb_port" {}
+variable "vault_alb_protocol" {}
+variable "vault_alb_ssl_policy" { default = "ELBSecurityPolicy-2016-08" }
+variable "consul_alb_port" {}
+variable "consul_alb_protocol" {}
+variable "consul_alb_ssl_policy" { default = "ELBSecurityPolicy-2016-08" }
+variable "nomad_alb_port" {}
+variable "nomad_alb_protocol" {}
+variable "nomad_alb_ssl_policy" { default = "ELBSecurityPolicy-2016-08" }
 variable "region" {}
 variable "priv_subnets" { type = "list" }
 variable "pub_subnets" { type = "list" }
-variable "asg_max" {}
-variable "asg_min" {}
-variable "asg_desired" {}
 variable "key_name" {}
-variable "app_name" {}
 variable "ami_id" {}
-variable "userdata" {}
-variable "instance_type" {}
-variable "asg_sgs" { type = "list" }
+variable "vault_asg_max" {}
+variable "vault_asg_min" {}
+variable "vault_asg_desired" {}
+variable "vault_userdata" {}
+variable "vault_instance_type" {}
+variable "vault_sgs" { type = "list" }
+variable "vault_iam_instance_profile" {}
+variable "consul_asg_max" {}
+variable "consul_asg_min" {}
+variable "consul_asg_desired" {}
+variable "consul_userdata" {}
+variable "consul_instance_type" {}
+variable "consul_sgs" { type = "list" }
+variable "consul_iam_instance_profile" {}
+variable "nomad_asg_max" {}
+variable "nomad_asg_min" {}
+variable "nomad_asg_desired" {}
+variable "nomad_userdata" {}
+variable "nomad_instance_type" {}
+variable "nomad_sgs" { type = "list" }
+variable "nomad_iam_instance_profile" {}
 variable "datacenter" {}
 variable "consul_server_join_tag_value" {}
 variable "consul_server_join_tag_key" {}
-variable "iam_instance_profile" {}
-variable "port" {}
-variable "protocol" {}
 variable "vpc_id" {}
-variable "hc_interval" {}
-variable "hc_path" {}
-variable "hc_port" {}
-variable "hc_protocol" {}
-variable "hc_timeout" {}
-variable "hc_healthy_threshold" {}
-variable "hc_unhealthy_threshold" {}
-variable "matcher" { default = "200"}
-variable "endpoint_url" {}
+variable "nomad_tg_port" {}
+variable "nomad_tg_protocol" {}
+variable "nomad_hc_interval" {}
+variable "nomad_hc_path" {}
+variable "nomad_hc_port" {}
+variable "nomad_hc_protocol" {}
+variable "nomad_hc_timeout" {}
+variable "nomad_hc_healthy_threshold" {}
+variable "nomad_hc_unhealthy_threshold" {}
+variable "nomad_matcher" { default = "200"}
+variable "consul_tg_port" {}
+variable "consul_tg_protocol" {}
+variable "consul_hc_interval" {}
+variable "consul_hc_path" {}
+variable "consul_hc_port" {}
+variable "consul_hc_protocol" {}
+variable "consul_hc_timeout" {}
+variable "consul_hc_healthy_threshold" {}
+variable "consul_hc_unhealthy_threshold" {}
+variable "consul_matcher" { default = "200"}
+variable "vault_tg_port" {}
+variable "vault_tg_protocol" {}
+variable "vault_hc_interval" {}
+variable "vault_hc_path" {}
+variable "vault_hc_port" {}
+variable "vault_hc_protocol" {}
+variable "vault_hc_timeout" {}
+variable "vault_hc_healthy_threshold" {}
+variable "vault_hc_unhealthy_threshold" {}
+variable "vault_matcher" { default = "200"}
+variable "vault_endpoint_url" {}
+variable "consul_endpoint_url" {}
+variable "nomad_endpoint_url" {}
 variable "r53_zone_id" {}
 
-output "asg" { value = "${aws_autoscaling_group.asg.id}" }
+output "vaultasg" { value = "${aws_autoscaling_group.vaultasg.id}" }
+output "consulasg" { value = "${aws_autoscaling_group.consulasg.id}" }
+output "nomadasg" { value = "${aws_autoscaling_group.nomadasg.id}" }
--- main.tf
+++ main.tf
@@ -1,10 +1,42 @@
-data "aws_acm_certificate" "cert" {
-  domain = "${var.acm_domain}"
+data "aws_acm_certificate" "vaultcert" {
+  domain = "${var.vault_acm_domain}"
 }
 
-resource "aws_route53_record" "r53" {
+data "aws_acm_certificate" "consulcert" {
+  domain = "${var.consul_acm_domain}"
+}
+
+data "aws_acm_certificate" "nomadcert" {
+  domain = "${var.nomad_acm_domain}"
+}
+
+resource "aws_route53_record" "vaultr53" {
   zone_id = "${var.r53_zone_id}"
-  name = "${var.endpoint_url}"
+  name = "${var.vault_endpoint_url}"
+  type = "A"
+
+  alias {
+    name = "${aws_alb.alb.dns_name}"
+    zone_id = "${aws_alb.alb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "consulr53" {
+  zone_id = "${var.r53_zone_id}"
+  name = "${var.consul_endpoint_url}"
+  type = "A"
+
+  alias {
+    name = "${aws_alb.alb.dns_name}"
+    zone_id = "${aws_alb.alb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "nomadr53" {
+  zone_id = "${var.r53_zone_id}"
+  name = "${var.nomad_endpoint_url}"
   type = "A"
 
   alias {
@@ -15,48 +47,106 @@ resource "aws_route53_record" "r53" {
 }
 
 resource "aws_alb" "alb" {
-  name_prefix = "${var.app_name}"
+  name_prefix = "hashistack"
   internal = false
-  security_groups = ["${var.asg_sgs}}"]
+  security_groups = ["${var.nomad_sgs}","${var.consul_sgs}","${var.vault_sgs}"]
   subnets = ["${var.pub_subnets}}"]
 }
 
-resource "aws_alb_target_group" "albtg" {
-  name = "${var.app_name}"
-  port = "${var.port}"
-  protocol = "${var.protocol}"
+resource "aws_alb_target_group" "vaulttg" {
+  name = "vault"
+  port = "${var.vault_tg_port}"
+  protocol = "${var.vault_tg_protocol}"
   vpc_id = "${var.vpc_id}"
   health_check {
-    interval = "${var.hc_interval}"
-    path = "${var.hc_path}"
-    port = "${var.hc_port}"
-    protocol = "${var.hc_protocol}"
-    timeout = "${var.hc_timeout}"
-    healthy_threshold = "${var.hc_healthy_threshold}"
-    unhealthy_threshold = "${var.hc_unhealthy_threshold}"
-    matcher = "${var.matcher}"
+    interval = "${var.vault_hc_interval}"
+    path = "${var.vault_hc_path}"
+    port = "${var.vault_hc_port}"
+    protocol = "${var.vault_hc_protocol}"
+    timeout = "${var.vault_hc_timeout}"
+    healthy_threshold = "${var.vault_hc_healthy_threshold}"
+    unhealthy_threshold = "${var.vault_hc_unhealthy_threshold}"
+    matcher = "${var.vault_matcher}"
   }
 }
 
-resource "aws_alb_listener" "albl" {
+resource "aws_alb_target_group" "consultg" {
+  name = "consul"
+  port = "${var.consul_tg_port}"
+  protocol = "${var.consul_tg_protocol}"
+  vpc_id = "${var.vpc_id}"
+  health_check {
+    interval = "${var.consul_hc_interval}"
+    path = "${var.consul_hc_path}"
+    port = "${var.consul_hc_port}"
+    protocol = "${var.consul_hc_protocol}"
+    timeout = "${var.consul_hc_timeout}"
+    healthy_threshold = "${var.consul_hc_healthy_threshold}"
+    unhealthy_threshold = "${var.consul_hc_unhealthy_threshold}"
+    matcher = "${var.consul_matcher}"
+  }
+}
+
+resource "aws_alb_target_group" "nomadtg" {
+  name = "nomad"
+  port = "${var.nomad_tg_port}"
+  protocol = "${var.nomad_tg_protocol}"
+  vpc_id = "${var.vpc_id}"
+  health_check {
+    interval = "${var.nomad_hc_interval}"
+    path = "${var.nomad_hc_path}"
+    port = "${var.nomad_hc_port}"
+    protocol = "${var.nomad_hc_protocol}"
+    timeout = "${var.nomad_hc_timeout}"
+    healthy_threshold = "${var.nomad_hc_healthy_threshold}"
+    unhealthy_threshold = "${var.nomad_hc_unhealthy_threshold}"
+    matcher = "${var.nomad_matcher}"
+  }
+}
+
+resource "aws_alb_listener" "vaultalbl" {
   load_balancer_arn = "${aws_alb.alb.arn}"
-  port = "${var.alb_port}"
-  protocol = "${var.alb_protocol}"
-  ssl_policy = "${var.ssl_policy}"
-  certificate_arn = "${aws_acm_certificate.cert.arn}"
+  port = "${var.vault_alb_port}"
+  protocol = "${var.vault_alb_protocol}"
+  ssl_policy = "${var.vault_alb_ssl_policy}"
+  certificate_arn = "${data.aws_acm_certificate.vaultcert.arn}"
   default_action {
-    target_group_arn = "${aws_alb_target_group.albtg.arn}"
+    target_group_arn = "${aws_alb_target_group.vaulttg.arn}"
     type = "forward"
   }
 }
 
-resource "aws_launch_configuration" "lc" {
-  name_prefix = "${var.app_name}"
+resource "aws_alb_listener" "consulalbl" {
+  load_balancer_arn = "${aws_alb.alb.arn}"
+  port = "${var.consul_alb_port}"
+  protocol = "${var.consul_alb_protocol}"
+  ssl_policy = "${var.consul_alb_ssl_policy}"
+  certificate_arn = "${data.aws_acm_certificate.consulcert.arn}"
+  default_action {
+    target_group_arn = "${aws_alb_target_group.consultg.arn}"
+    type = "forward"
+  }
+}
+
+resource "aws_alb_listener" "nomadalbl" {
+  load_balancer_arn = "${aws_alb.alb.arn}"
+  port = "${var.nomad_alb_port}"
+  protocol = "${var.nomad_alb_protocol}"
+  ssl_policy = "${var.nomad_alb_ssl_policy}"
+  certificate_arn = "${data.aws_acm_certificate.nomadcert.arn}"
+  default_action {
+    target_group_arn = "${aws_alb_target_group.nomadtg.arn}"
+    type = "forward"
+  }
+}
+
+resource "aws_launch_configuration" "vaultlc" {
+  name_prefix = "vault"
   image_id = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  user_data = "${var.userdata}"
-  iam_instance_profile = "${var.iam_instance_profile}"
-  security_groups = ["${var.asg_sgs}"]
+  instance_type = "${var.vault_instance_type}"
+  user_data = "${var.vault_userdata}"
+  iam_instance_profile = "${var.vault_iam_instance_profile}"
+  security_groups = ["${var.vault_sgs}"]
   key_name = "${var.key_name}"
 
   root_block_device {
@@ -69,20 +159,104 @@ resource "aws_launch_configuration" "lc" {
   }
 }
 
-resource "aws_autoscaling_group" "asg" {
-  name = "${aws_launch_configuration.lc.name}"
-  launch_configuration  = "${aws_launch_configuration.lc.name}"
+resource "aws_autoscaling_group" "vaultasg" {
+  name = "${aws_launch_configuration.vaultlc.name}"
+  launch_configuration  = "${aws_launch_configuration.vaultlc.name}"
   vpc_zone_identifier = ["${var.priv_subnets}"]
-  max_size = "${var.asg_max}"
-  min_size = "${var.asg_min}"
-  desired_capacity = "${var.asg_desired}"
+  max_size = "${var.vault_asg_max}"
+  min_size = "${var.vault_asg_min}"
+  desired_capacity = "${var.vault_asg_desired}"
   health_check_grace_period = 300
   health_check_type = "EC2"
   force_delete = true
-  target_group_arns = ["${aws_alb_target_group.albtg.arn}"]
+  target_group_arns = ["${aws_alb_target_group.vaulttg.arn}"]
   tag {
     key = "Name"
-    value = "${var.app_name}"
+    value = "vault"
+    propagate_at_launch = true
+  }
+  tag {
+    key = "${var.consul_server_join_tag_key}"
+    value = "${var.consul_server_join_tag_value}"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "nomadlc" {
+  name_prefix = "nomad"
+  image_id = "${var.ami_id}"
+  instance_type = "${var.nomad_instance_type}"
+  user_data = "${var.nomad_userdata}"
+  iam_instance_profile = "${var.nomad_iam_instance_profile}"
+  security_groups = ["${var.nomad_sgs}"]
+  key_name = "${var.key_name}"
+
+  root_block_device {
+    volume_size = 30
+    delete_on_termination = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "nomadasg" {
+  name = "${aws_launch_configuration.nomadlc.name}"
+  launch_configuration  = "${aws_launch_configuration.nomadlc.name}"
+  vpc_zone_identifier = ["${var.priv_subnets}"]
+  max_size = "${var.nomad_asg_max}"
+  min_size = "${var.nomad_asg_min}"
+  desired_capacity = "${var.nomad_asg_desired}"
+  health_check_grace_period = 300
+  health_check_type = "EC2"
+  force_delete = true
+  target_group_arns = ["${aws_alb_target_group.nomadtg.arn}"]
+  tag {
+    key = "Name"
+    value = "nomad"
+    propagate_at_launch = true
+  }
+  tag {
+    key = "${var.consul_server_join_tag_key}"
+    value = "${var.consul_server_join_tag_value}"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "consullc" {
+  name_prefix = "consul"
+  image_id = "${var.ami_id}"
+  instance_type = "${var.consul_instance_type}"
+  user_data = "${var.consul_userdata}"
+  iam_instance_profile = "${var.consul_iam_instance_profile}"
+  security_groups = ["${var.consul_sgs}"]
+  key_name = "${var.key_name}"
+
+  root_block_device {
+    volume_size = 30
+    delete_on_termination = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "consulasg" {
+  name = "${aws_launch_configuration.consullc.name}"
+  launch_configuration  = "${aws_launch_configuration.consullc.name}"
+  vpc_zone_identifier = ["${var.priv_subnets}"]
+  max_size = "${var.consul_asg_max}"
+  min_size = "${var.consul_asg_min}"
+  desired_capacity = "${var.consul_asg_desired}"
+  health_check_grace_period = 300
+  health_check_type = "EC2"
+  force_delete = true
+  target_group_arns = ["${aws_alb_target_group.consultg.arn}"]
+  tag {
+    key = "Name"
+    value = "consul"
     propagate_at_launch = true
   }
   tag {
